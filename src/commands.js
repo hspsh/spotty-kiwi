adad1a71091e0172bcb2fec70dfc75a823f0839a
LiquidLemon <liquidlemon@protonmail.com>
2018-07-02 11:30:28 +0200

    Configure ESLint
    Using mostly standard rules so they will be tweaked along the way.

--- src/commands.js
+++ src/commands.js
@@ -4,11 +4,11 @@ const formatStatus = data => {
   if (data.headcount === 0) {
     return 'Hackerspace jest pusty'
   }
-  
+
   if (data.headcount === 1) {
-    return `W spejsie jest jedna osoba: ${data.users[0]}` 
+    return `W spejsie jest jedna osoba: ${data.users[0]}`
   }
-  
+
   // TODO: extract pluralization logic
   const units = data.headcount % 10
   const [verb, noun] = units >= 2 && units <= 4
@@ -18,10 +18,10 @@ const formatStatus = data => {
 }
 
 module.exports = [
-  { 
+  {
     name: '/ktohakuje',
     waitMessage: 'Rozpoczynam skanowanie spejsu...',
-    handler: async req => {
+    handler: async _req => {
       const data = (await axios.get('https://at.hs3.pl/api/now')).data
       return formatStatus(data)
     }
