a46cb2a953adc49a1d28869b35bfe860b1bc3db7
psuwala <oneat@vp.pl>
2021-12-31 00:05:11 +0100

    added plugin for rel!

--- lib/plugins/rel/controller/DiscordController.ts
+++ lib/plugins/rel/controller/DiscordController.ts
@@ -1,48 +1,52 @@
 import { Message } from 'discord.js'
 import { JudgementService } from '../service/JudgementService'
 
-export class DiscordController{
+export class DiscordController {
+    constructor(public judgementService: JudgementService) {}
 
-    constructor(public judgementService: JudgementService){
-
-    }
-    
-    async onMessage(message: Message){
-        if(!this.matchesMessage(message)){
+    async onMessage(message: Message): Promise<void> {
+        if (!this.matchesMessage(message)) {
             return
         }
 
-        const [_, category, actionLiteral] : [string, string, string]  = message.content.match(DiscordController.REGEX) as [string ,string,string] 
+        const [, category, actionLiteral]: [string, string, string] =
+            message.content.match(DiscordController.REGEX) as [
+                string,
+                string,
+                string
+            ]
         const pointsToAdd = actionLiteral.length
 
         const judgeId: string = this.extractJudgeId(message)
-        const judgedId: string = this.extractJudgedId(message)!
-        const judgedUsername: string = this.extractJudgedUsername(message)!
+        const judgedId: string = this.extractJudgedId(message) || ''
+        const judgedUsername: string = this.extractJudgedUsername(message) || ''
 
         await this.judgementService.judge({
             judgingUserID: judgeId,
             category: category,
             judgedUserID: judgedId,
-            points: pointsToAdd
+            points: pointsToAdd,
         })
 
         const points = await this.judgementService.retrieveJudgementPoints({
             judgingUserID: judgeId,
             judgedUserID: judgedId,
-            category: category
+            category: category,
         })
 
-        await this.respond(message, judgedUsername, points, category) 
+        await this.respond(message, judgedUsername, points, category)
     }
 
     matchesMessage(message: Message): boolean {
         const isSentAsReply = !!message.interaction
-        const matchesJudgementPattern = !!message?.content?.match(DiscordController.REGEX)
+        const matchesJudgementPattern = !!message?.content?.match(
+            DiscordController.REGEX
+        )
 
         return isSentAsReply && matchesJudgementPattern
     }
 
-    extractJudgeId(message: Message): string{
+    extractJudgeId(message: Message): string {
         return message.author.id
     }
 
@@ -54,11 +58,15 @@ export class DiscordController{
         return message.interaction?.user.username
     }
 
-    async respond(message: Message, judgedUsername: string, points: number, category: string): Promise<void>{
+    async respond(
+        message: Message,
+        judgedUsername: string,
+        points: number,
+        category: string
+    ): Promise<void> {
         const reply = `Członek ${judgedUsername} posiaga ${points} w kategorii ${category}`
         await message.reply(reply)
     }
 
-    static REGEX = /^([a-zA-Z]+)(\++|\-+)$/
-
+    static REGEX = /^([a-zA-Z]+)(\++|-+)$/
 }
---
+++ lib/plugins/rel/plugoin.ts
@@ -0,0 +1,45 @@
+import { Connection } from 'typeorm'
+import { createConnection } from 'typeorm'
+import { DiscordController } from './controller/DiscordController'
+
+import { JudgedMember } from './domain/JudgedMember'
+import { JudgementCategory } from './domain/JudgementCategory'
+import { JudgingMember } from './domain/JudgingMember'
+import { JudgementService } from './service/JudgementService'
+import { TypeORMJudgingMemberRepository } from './repository/TypeORMJudgingMemberRepository'
+
+import { Plugin } from '../pluginManager'
+import { Message } from 'discord.js'
+
+export class PluginFactory {
+    async createPlugin(dbPath: string): Promise<Plugin> {
+        const connection = await this.createConnection(dbPath)
+        const controller = new DiscordController(
+            new JudgementService(
+                new TypeORMJudgingMemberRepository(connection.manager)
+            )
+        )
+
+        return {
+            name: 'judgement',
+            commands: [],
+            messageHandlers: [
+                {
+                    action: (msg: Message) => controller.onMessage(msg),
+                    predicate: async (msg) => controller.matchesMessage(msg),
+                },
+            ],
+        }
+    }
+
+    async createConnection(forDBPath: string): Promise<Connection> {
+        const connection = await createConnection({
+            type: 'sqlite',
+            database: forDBPath,
+            entities: [JudgedMember, JudgementCategory, JudgingMember],
+            logging: ['log'],
+        })
+        await connection.synchronize()
+        return connection
+    }
+}
